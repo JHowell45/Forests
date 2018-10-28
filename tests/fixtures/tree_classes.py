0d292c9fa0abfcc68f33920c561d5f9c50b9755c
Unknown <jhowell45@googlemail.com>
2018-10-28 16:21:45 +0000

    Updating tests to use arams for fixtures.

--- tests/fixtures/tree_classes.py
+++ tests/fixtures/tree_classes.py
@@ -6,6 +6,20 @@ in the 'tree_classes' file.
 import pytest
 from forests.tree_classes import TreeNode
 
+payload_data = [
+    'test payload',
+    10,
+    23.54,
+    True
+]
+
+payload_ids = [
+    'string',
+    'integer',
+    'float',
+    'boolean'
+]
+
 
 @pytest.fixture()
 def test_tree_node_empty():
@@ -20,22 +34,24 @@ def test_tree_node_empty():
     return TreeNode()
 
 
-@pytest.fixture()
-def test_tree_node_payload():
+@pytest.fixture(params=payload_data, ids=payload_ids)
+def test_tree_node_payload(request):
     """use this function to define a test payload value for a TreeNode.
 
     This function is used for defining a test payload value that can be
     passed to a 'TreeNode' instance. This will help for when testing the
     payload value for a
 
-    :return:
+    :return: a test payload value.
+    :rtype:
     """
-    return 'test payload'
+    return request.param
 
 
 @pytest.fixture()
 def test_tree_node_no_parent_and_child(test_tree_node_payload):
     return TreeNode(payload=test_tree_node_payload)
+
 
 @pytest.fixture()
 def test_tree_node_no_child(test_tree_node_payload,
