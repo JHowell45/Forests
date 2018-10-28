2e8b9715fbb2be37bb83c830abda4538cb6ebd56
Unknown <jhowell45@googlemail.com>
2018-10-28 17:41:33 +0000

    Updated tests.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,13 @@ This file is used for loading all of the test fixtures for use within any of
 the test classes.
 """
 from tests.fixtures.tree_classes import (
+    test_tree_node_base,
+    test_tree_node_child,
     test_tree_node_empty,
     test_tree_node_no_child,
-    test_tree_node_no_parent_and_child,
+    test_tree_node_parent
+)
+from tests.fixtures.tree_node_parameter_data import (
+    test_tree_node_id,
     test_tree_node_payload
 )
--- tests/fixtures/tree_classes.py
+++ tests/fixtures/tree_classes.py
@@ -6,20 +6,6 @@ in the 'tree_classes' file.
 import pytest
 from forests.tree_classes import TreeNode
 
-payload_data = [
-    'test payload',
-    10,
-    23.54,
-    True
-]
-
-payload_ids = [
-    'string',
-    'integer',
-    'float',
-    'boolean'
-]
-
 
 @pytest.fixture()
 def test_tree_node_empty():
@@ -34,26 +20,55 @@ def test_tree_node_empty():
     return TreeNode()
 
 
-@pytest.fixture(params=payload_data, ids=payload_ids)
-def test_tree_node_payload(request):
-    """use this function to define a test payload value for a TreeNode.
+@pytest.fixture()
+def test_tree_node_base(test_tree_node_payload):
+    """Use this fixture to create a base TreeNode instance.
 
-    This function is used for defining a test payload value that can be
-    passed to a 'TreeNode' instance. This will help for when testing the
-    payload value for a
+    This fixture is used for creating an instance of the 'TreeNode' class
+    with only the payload provided.
 
-    :return: a test payload value.
-    :rtype:
+    :param test_tree_node_payload: the test payload value.
+    :type test_tree_node_payload:
+    :return: the test 'TreeNode' instance with only a payload.
+    :rtype: TreeNode
     """
-    return request.param
+    return TreeNode(payload=test_tree_node_payload)
 
 
 @pytest.fixture()
-def test_tree_node_no_parent_and_child(test_tree_node_payload):
+def test_tree_node_parent(test_tree_node_payload):
+    """Use this fixture to create a parent TreeNode instance.
+
+    This fixture is used for creating an instance of the 'TreeNode' class
+    with only the payload provided.
+
+    :param test_tree_node_payload: the test payload value.
+    :type test_tree_node_payload:
+    :return: the test 'TreeNode' instance with only a payload.
+    :rtype: TreeNode
+    """
+    return TreeNode(payload=test_tree_node_payload)
+
+
+@pytest.fixture()
+def test_tree_node_child(test_tree_node_payload):
+    """Use this fixture to create a child TreeNode instance.
+
+    This fixture is used for creating an instance of the 'TreeNode' class
+    with only the payload provided.
+
+    :param test_tree_node_payload: the test payload value.
+    :type test_tree_node_payload:
+    :return: the test 'TreeNode' instance with only a payload.
+    :rtype: TreeNode
+    """
     return TreeNode(payload=test_tree_node_payload)
 
 
 @pytest.fixture()
 def test_tree_node_no_child(test_tree_node_payload,
-                            test_tree_node_no_parent_and_child):
-    return TreeNode()
+                            test_tree_node_parent):
+    return TreeNode(
+        payload=test_tree_node_payload,
+        parent=test_tree_node_parent
+    )
