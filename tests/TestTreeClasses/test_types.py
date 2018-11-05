47df143422a8deccb08280ec2be17726d311dd90
Unknown <jhowell45@googlemail.com>
2018-11-05 20:45:53 +0000

    adding docs to the unit tests.

--- tests/TestTreeClasses/test_types.py
+++ tests/TestTreeClasses/test_types.py
@@ -5,6 +5,7 @@ tree_classes' file.
 """
 import pytest
 
+
 class TestTreeNode:
 	"""Use this class to test the 'TreeNode' class.
 
@@ -13,5 +14,28 @@ class TestTreeNode:
 	"""
 
 	@pytest.mark.usefixtures('test_tree_node_base')
-	def test_empty_tree_node_id(self, test_tree_node_base):
+	def test_base_tree_node_id(self, test_tree_node_base):
+		"""Use this function to test the fixture ID value.
+
+		This function is used for testing the type for the base test
+		'TreeNode' instances ID value.
+
+		:param test_tree_node_base: the test base 'TreeNode' instance.
+		:type test_tree_node_base: TreeNode
+		"""
 		assert isinstance(getattr(test_tree_node_base, 'node_id'), int)
+
+	@pytest.mark.usefixtures('test_tree_node_base')
+	def test_base_tree_node_payload(self, test_tree_node_base):
+		"""Use this function to test the fixture payload value.
+
+		This function is used for testing the type for the base test
+		'TreeNode' instances payload value.
+
+		:param test_tree_node_base: the test base 'TreeNode' instance.
+		:type test_tree_node_base: TreeNode
+		"""
+		assert isinstance(
+				getattr(test_tree_node_base, 'payload'),
+				(int, str, float)
+		)
