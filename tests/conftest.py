5f63e97a67ae6b09465dc76604fb71f0028f1380
JHowell45 <jhowell45@googlemail.com>
2018-12-20 21:04:22 +0000

    Updated docs.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,5 @@
 """Use this file to import all of the test fixtures.
 
-This file is used for loading all of the test fixtures for use within any of
-the test classes.
+This file is used for loading all of the test fixtures for use within any of the test
+classes.
 """
