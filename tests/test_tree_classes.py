4e8e9145c9fb54b7417c3611ed66a9b5c5cf76ef
JHowell45 <jhowell45@googlemail.com>
2018-12-20 22:03:50 +0000

    Adding docs to tests.

--- tests/test_tree_classes.py
+++ tests/test_tree_classes.py
@@ -1,11 +1,33 @@
 class TestTreeNodeGetterFunctions:
+    """Use this class to test the 'TreeNode' class to test the getters made.
+
+    This function is used for testing the getters for the 'TreeNode' class. This
+    tests to make sure the getters return the correct values and types.
+    """
+
     def test_id_type(self, tree_node_instance):
+        """Use this function to test the type of the ID for the 'TreeNode' instance.
+
+        This function is used for testing the type for the ID value for the test
+        'TreeNode' instance.
+
+        :param tree_node_instance: the test 'TreeNode' instance.
+        :type tree_node_instance: TreeNode
+        """
         assert isinstance(getattr(tree_node_instance, "id"), int)
 
     def test_id_value(self, tree_node_instance, tree_node_instance_data):
         assert getattr(tree_node_instance, "id") == tree_node_instance_data["id"]
 
     def test_payload_type(self, tree_node_instance):
+        """Use this function to test the type of the payload for 'TreeNode'.
+
+        This function is used for testing the type for the payload value for the test
+        'TreeNode' instance.
+
+        :param tree_node_instance: the test 'TreeNode' instance.
+        :type tree_node_instance: TreeNode
+        """
         assert isinstance(getattr(tree_node_instance, "payload"), int)
 
     def test_payload_value(self, tree_node_instance, tree_node_instance_data):
@@ -14,6 +36,14 @@ class TestTreeNodeGetterFunctions:
         )
 
     def test_children_type(self, tree_node_instance):
+        """Use this function to test the type of the children for 'TreeNode'.
+
+        This function is used for testing the type for the children value for the test
+        'TreeNode' instance.
+
+        :param tree_node_instance: the test 'TreeNode' instance.
+        :type tree_node_instance: TreeNode
+        """
         assert isinstance(getattr(tree_node_instance, "children"), int)
 
     def test_children_value(self, tree_node_instance, tree_node_instance_data):
@@ -23,6 +53,14 @@ class TestTreeNodeGetterFunctions:
         )
 
     def test_parent_type(self, tree_node_instance):
+        """Use this function to test the type of the parent for 'TreeNode'.
+
+        This function is used for testing the type for the parent value for the test
+        'TreeNode' instance.
+
+        :param tree_node_instance: the test 'TreeNode' instance.
+        :type tree_node_instance: TreeNode
+        """
         assert isinstance(getattr(tree_node_instance, "parent"), int)
 
     def test_parent_value(self, tree_node_instance, tree_node_instance_data):
@@ -36,4 +74,8 @@ class TestTreeNodeSetterFunctions:
 
 
 class TestTreeNodeClassMethods:
+    pass
+
+
+class TestTreeNodeMethods:
     pass
