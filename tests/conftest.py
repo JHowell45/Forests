bf48aedb0283b5a643cd9b0bcfb6f8f52a7ea19f
JHowell45 <jhowell45@googlemail.com>
2018-12-20 21:02:25 +0000

    removed old fixtures from conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,14 +3,3 @@
 This file is used for loading all of the test fixtures for use within any of
 the test classes.
 """
-from tests.fixtures.tree_classes import (
-    test_tree_node_base,
-    test_tree_node_child,
-    test_tree_node_empty,
-    test_tree_node_no_child,
-    test_tree_node_parent
-)
-from tests.fixtures.tree_node_parameter_data import (
-    test_tree_node_id,
-    test_tree_node_payload
-)
