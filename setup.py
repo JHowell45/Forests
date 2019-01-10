2b9a8566296ca4312e4425aaeb10b649eaa376e3
JHowell45 <jhowell45@googlemail.com>
2019-01-10 21:02:50 +0000

    Updated version to be lower.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="forests",
-    version="0.1.0",
+    version="0.0.1",
     url="https://github.com/JHowell45/Forests",
     author="Jacob Howell",
     author_email="email@gmail.com",
