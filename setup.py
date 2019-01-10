d2970d351eda91478c119d1df6bd3246b125bd5b
JHowell45 <jhowell45@googlemail.com>
2019-01-10 20:26:34 +0000

    Fixed error in file.

--- setup.py
+++ setup.py
@@ -7,15 +7,13 @@ setup(
     author="Jacob Howell",
     author_email="email@gmail.com",
     description="Pre-made Tree data structure.",
-    long_description=open('README.md').read(),
-    packages=['forests'],
+    long_description=open("README.md").read(),
+    packages=["forests"],
     install_requires=[],
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 2 - Pre-Alpha",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
     ],
-
-)
 )
