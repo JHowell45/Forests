607799f9cae0f862c304b5e2cd8177ad4c2d1259
JHowell45 <jhowell45@googlemail.com>
2019-04-23 19:37:25 +0100

    Updating tests.

--- tests/fixtures/tree_classes.py
+++ tests/fixtures/tree_classes.py
@@ -7,7 +7,7 @@ test_data_ids = [str(type(data)) for data in test_data]
 
 
 @pytest.fixture(params=test_data, ids=test_data_ids)
-def tree_node_instance_data(request):
+def tree_node_instance_data(request) -> dict:
     """Use this fixture to create test data for creating the test 'TreeNode' fixtures.
 
     This function is used for generating the dictionary used for creating the test
@@ -15,21 +15,33 @@ def tree_node_instance_data(request):
 
     :param request: allows for several values to be passed as params.
     :return: test TreeNode data.
-    :rtype: dict
     """
     return {"node_id": 0, "payload": request.param, "children": None, "parent": None}
 
 
 @pytest.fixture()
-def tree_node_instance(tree_node_instance_data):
+def tree_node_instance(tree_node_instance_data) -> TreeNode:
     """Use this function to generate the test 'TreeNode' instance.
 
     This function is used for generating the test 'TreeNode' instance for use for
     testing the attributes and functions.
 
-    :param tree_node_instance_data: the test 'TreeNode' data.
-    :type tree_node_instance_data: dict
+    :param tree_node_instance_data: test 'TreeNode' data.
     :return: the test 'TreeNode' instance.
-    :rtype: TreeNode
     """
     return TreeNode(**tree_node_instance_data)
+
+
+@pytest.fixture(params=test_data, ids=test_data_ids)
+def tree_node_instance_with_parent_data(request):
+    return {
+        "node_id": 1,
+        "payload": request.param,
+        "children": None,
+        "parent": {
+            "node_id": 0,
+            "payload": request.param,
+            "children": None,
+            "parent": None,
+        },
+    }
